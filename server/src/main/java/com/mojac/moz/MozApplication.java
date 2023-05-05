20844b196eb9c86966b954df2b2c66c161780c74
hhyuw <high2092@gmail.com>
2023-05-05 21:17:08 +0900

    Initialize Spring Boot Project

---
+++ server/src/main/java/com/mojac/moz/MozApplication.java
@@ -0,0 +1,13 @@
+package com.mojac.moz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MozApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/mojac/moz/MozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mojac.moz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
