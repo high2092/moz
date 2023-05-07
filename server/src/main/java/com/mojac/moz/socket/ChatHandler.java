ab5c2dd5458ee39b1d351c09ae70c044b8e207d3
hhyuw <high2092@gmail.com>
2023-05-08 01:21:44 +0900

    [BE] feat: 채팅

---
+++ server/src/main/java/com/mojac/moz/socket/ChatHandler.java
@@ -0,0 +1,40 @@
+package com.mojac.moz.socket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class ChatHandler extends TextWebSocketHandler {
+
+    private List<WebSocketSession> sessions = new ArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("connect {}", session);
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload {}", payload);
+
+        for (WebSocketSession s : sessions) {
+            s.sendMessage(message);
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("disconnect {}", session);
+        sessions.remove(session);
+    }
+}
