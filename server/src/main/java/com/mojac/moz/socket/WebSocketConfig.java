40f29db55ba0f5b561a49c54555f2d98d947bd83
hhyuw <high2092@gmail.com>
2023-05-10 12:42:11 +0900

    [BE] fix: Bean 순환 종속성 문제 해결

--- server/src/main/java/com/mojac/moz/socket/WebSocketConfig.java
+++ server/src/main/java/com/mojac/moz/socket/WebSocketConfig.java
@@ -1,8 +1,6 @@
 package com.mojac.moz.socket;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -19,10 +17,5 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(chatHandler, "/my-handler")
                 .setAllowedOrigins("*");
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
     }
 }
