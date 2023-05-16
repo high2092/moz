cc66338c5271a74d4531a69079519babde7ddf3c
hhyuw <high2092@gmail.com>
2023-05-16 22:41:55 +0900

    [FE] feat: 문제집으로 문제 추가하기

--- client/src/components/AddQuizModal.tsx
+++ client/src/components/AddQuizModal.tsx
@@ -10,16 +10,27 @@ export const AddQuizModal = ({ zIndex }: PreparedModalProps) => {
   return <CenteredModal content={<AddQuizModalContent />} zIndex={zIndex} />;
 };
 
-function AddQuizModalContent() {
-  const { quizzes } = useAppSelector((state) => state.moz);
-  const [addQuizList, setAddQuizList] = useState<Quiz[]>([]);
+interface AddQuiz {
+  id?: number;
+  quizList?: Quiz[];
+}
 
-  const handleQuizClick = (quiz: Quiz) => {
+function AddQuizModalContent() {
+  const { quizzes, quizBundles } = useAppSelector((state) => state.moz);
+  const [addQuizList, setAddQuizList] = useState<AddQuiz[]>([]);
+
+  const handleQuizClick = (quiz: AddQuiz) => {
     setAddQuizList((addQuizList) => addQuizList.concat(quiz));
   };
 
   const handleApplyButtonClick = async () => {
-    const response = await httpPostApi('game/add-quiz', { quizList: addQuizList.map((quiz) => quiz.id) });
+    const quizList = addQuizList.reduce((acc, { id, quizList }) => {
+      if (quizList) return acc.concat(quizList.map(({ id }) => id));
+      return id;
+    }, []);
+
+    const response = await httpPostApi('game/add-quiz', { quizList });
+
     if (!response.ok) {
       console.error(response.statusText);
       return;
@@ -28,20 +39,38 @@ function AddQuizModalContent() {
 
   return (
     <S.AddQuizModal>
-      <div>
-        {Object.values(quizzes).map((quiz) => {
-          const { id, question, answers } = quiz;
-          return (
-            <div key={id} onClick={() => handleQuizClick(quiz)}>
-              {id} {question} {answers[0].answer}
-            </div>
-          );
-        })}
+      <div style={{ display: 'flex', width: '30vw' }}>
+        <div style={{ flex: 1 }}>
+          <h1>문제</h1>
+          <div>
+            {Object.values(quizzes).map((quiz) => {
+              const { id, question, answers } = quiz;
+              return (
+                <div key={`quiz-${id}`} onClick={() => handleQuizClick(quiz)}>
+                  {id} {question} {answers[0].answer}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div style={{ flex: 1 }}>
+          <h1>문제집</h1>
+          <div>
+            {Object.values(quizBundles).map((quizBundle) => {
+              const { id, title } = quizBundle;
+              return (
+                <div key={`quizBundle-${id}`} onClick={() => handleQuizClick(quizBundle)}>
+                  {title}
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
       <hr />
       <div style={{ display: 'flex' }}>
-        {addQuizList.map(({ id }) => (
-          <div key={`addQuiz-${id}`} style={{ margin: '0 2px' }}>
+        {addQuizList.map(({ id, quizList }) => (
+          <div key={`addQuiz-${id}`} style={{ margin: '0 2px', background: quizList ? 'orange' : 'yellow' }}>
             {id}
           </div>
         ))}
